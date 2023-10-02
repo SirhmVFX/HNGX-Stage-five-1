29928184b614086df56cdaa8dba905e85834c49c
sirhmvfx <sirhmvfx@gmail.com>
2023-10-02 14:33:27 +0100

    hmmmmm

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,6 +17,19 @@ function Header() {
                 <li>
                   <Link>How it works</Link>
                 </li>
+                <li>
+                  <Link to="/login">Login</Link>
+                </li>
+                <li>
+                  <Link to="/sign-up">Sign Up</Link>
+                </li>
+
+                <li>
+                  <Link to="/dashboard">Video Dashboard</Link>
+                </li>
+                <li>
+                  <Link to="/newvideo/:id">Video Dashboard</Link>
+                </li>
               </ul>
             </div>
 
